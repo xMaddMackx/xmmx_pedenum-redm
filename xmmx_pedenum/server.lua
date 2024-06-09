a6497a6178350e6369f4681cf96a8b0b57f111bf
xMaddMackx <82854182+xMaddMackx@users.noreply.github.com>
2024-06-09 03:10:54 -0400

    Update server.lua

--- xmmx_pedenum/server.lua
+++ xmmx_pedenum/server.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(5 * 60000) -- Check every 5 minutes (300000 milliseconds)
+        Citizen.Wait(30 * 60000) -- Check every 60 minutes (60 = 1 hour)
 
         local players = GetPlayers()
         local playerFound = false
